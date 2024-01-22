7a739c4ea674b086fed1914629db771eaf6ffcd6
itbergl <tberg644@gmail.com>
2024-01-22 15:39:01 +0800

    fixed authentication for /exchange endpoint

--- MarketMakerAPI/Program.cs
+++ MarketMakerAPI/Program.cs
@@ -52,7 +52,7 @@ builder.Services.AddAuthentication(x =>
         {
             var accessToken = context.Request.Query["access_token"];
             var path = context.HttpContext.Request.Path;
-            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/market"))
+            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/exchange"))
             {
                 context.Token = accessToken;
             }
