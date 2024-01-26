024f0b66d945eb895e1c4055c3af1bd6e0e520d9
Djimon <developerdjay@gmail.com>
2024-01-26 14:15:21 +0800

    added login function

--- python-cli/terminal.py
+++ python-cli/terminal.py
@@ -1,1 +1,34 @@
 # First version: log in, view all exchanges, update state of exchange, log out
+
+from dotenv import load_dotenv
+import os
+import requests
+import json
+
+# load the .env file
+load_dotenv()
+
+def login():
+    # returns the token if successful, None otherwise
+    url = "https://market-maker.azurewebsites.net/login"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "email": os.getenv("EMAIL"),
+        "password": os.getenv("PASSWORD")
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        print("Login successful!")
+        return response.content.decode("utf-8")
+    else:
+        print(f"Failed to login. Status code: {response.status_code}")
+        print(f"Response content: {response.content.decode('utf-8')}")
+        return None
+
+# TESTS
+assert(login() != None)
